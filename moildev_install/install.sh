bdc241d7eaded9b423ca4a5592863dca9740c1ce
yourskc <skc1125@gmail.com>
2019-11-28 17:35:51 +0800

    update

--- moildev_install/install.sh
+++ moildev_install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 /usr/lib/qt5/bin/qmake -install qinstall -exe libmoildev.so.1.0.0 /usr/lib/libmoildev.so.1.0.0
-ln -f -s libmoildev.so.1.0.0 /usr/lib/libmoildev.so
-ln -f -s libmoildev.so.1.0.0 /usr/lib/libmoildev.so.1
-ln -f -s libmoildev.so.1.0.0 /usr/lib/libmoildev.so.1.0
+ln -f -s /usr/lib/libmoildev.so.1.0.0 /usr/lib/libmoildev.so
+ln -f -s /usr/lib/libmoildev.so.1.0.0 /usr/lib/libmoildev.so.1
+ln -f -s /usr/lib/libmoildev.so.1.0.0 /usr/lib/libmoildev.so.1.0
 
 
 
